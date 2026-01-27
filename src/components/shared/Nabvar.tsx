a1bfcda1da918cf6018957e5037797ed19c1f3cb
mzajbe <mzajbe@gmail.com>
2026-01-27 12:09:42 +0600

    fix issue in navbar

--- src/components/shared/Nabvar.tsx
+++ src/components/shared/Nabvar.tsx
@@ -60,7 +60,7 @@ const Nabvar = () => {
           : 'bg-transparent'
       }`}
     >
-      <div className="max-w-7xl mx-auto px-4 pr-6 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto">
         <div className="flex items-center justify-between h-16 md:h-20">
           {/* Logo */}
           <div className="flex-shrink-0 flex items-center group">
